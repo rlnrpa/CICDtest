248b0bacdfa5f7ab20c363b0a1568ca0098ffb21
rlnrpa <rl@nordicrpa.com>
2023-10-31 14:26:17 +0200

    Added changes to EnableIncognitoMode.xaml
    Added changes to EnableIncognitoMode.xaml

---
+++ EnableIncognitoMode/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace EnableIncognitoMode
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
